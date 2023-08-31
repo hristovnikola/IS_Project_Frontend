602d1a38999066d042fe474f59b4796025c1a666
hristovnikola <hristov.nikola@yahoo.com>
2023-08-31 23:50:42 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ProductList from "./pages/Products/ProductsList/ProductsList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Aux from "./hoc/AuxWrapper"
+import AllRoutes from "./routes";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Aux>
+          {<AllRoutes/>}
+      </Aux>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Aux from './hoc/AuxWrapper'
+import {ToastContainer} from "react-bootstrap";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Aux>
+      <ToastContainer/>
+      <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+      </BrowserRouter>
+  </Aux>
 );
 
 // If you want to start measuring performance in your app, pass a function
